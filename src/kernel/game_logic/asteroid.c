f788cf648a9c724ff1e5428483975fd4cf9251c6
lagunovLev <lagunovl255@gmail.com>
2023-12-30 17:53:11 +0400

    sad

--- src/kernel/game_logic/asteroid.c
+++ src/kernel/game_logic/asteroid.c
@@ -22,7 +22,7 @@ void asteroid_constructor(Asteroid* this, vec pos, vec velocity, vec acceleratio
     this->strong = strong;
     this->size = size;
     this->damaged = 0;
-    this->verticies_count = size * PI / 3;
+    this->verticies_count = size * PI / 2.3;
     this->angle = 0;
     this->rotation_speed = rand_float(-0.02, 0.02);
     if (this->verticies_count % 2 != 0) this->verticies_count++;
