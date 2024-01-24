04422eb79d38c7207f04c280627a194774976e69
lagunovLev <lagunovl255@gmail.com>
2024-01-24 23:43:44 +0400

    kal

--- create_img/main.cpp
+++ create_img/main.cpp
@@ -82,12 +82,12 @@ int main(int argc, char* argv[])
     }
 
     mbr.partition[0] = {
-        .boot_indicator = 0,
+        .boot_indicator = 0x80,
         .starting_chs = { 0x00, 0x00, 0x02 },
         .os_type = 0xEE,
-        .ending_chs = { 0xFF, 0xFF, 0xFF },
+        .ending_chs = { 0b11111110, 0b11111011, 0b11111110 },
         .starting_lba = 0x00000001,
-        .size_lbas = 0xFFFFFFFF,
+        .size_lbas = 0x00000400,
     };
 
     ofstream output(dir + output_file_name, ios::binary | ios::out);
